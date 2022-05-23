a1140b8ea431930c459ea06edeb85df4fb118b74
Pikachupy <kala22051997@gmail.com>
2022-05-24 01:01:51 +0200

    Update: background

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
+                <div color="green">
                 <InputForm defaultValue={'hello form'}/>
+                </div>
             </header>
         </div>
     );
