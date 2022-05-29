f4ae357ebbad5ffd0eb2c97a220481046d6115d1
Pikachupy <kala22051997@gmail.com>
2022-05-29 20:56:55 +0200

    First commit on new branch test

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
+import {Routes, Route, Link} from "react-router-dom";
 import './App.css';
 import {InputForm} from "./InputForm";
+import {PolicyPage} from "./policy";
+
+export const routesPaths = {
+    about: 'about'
+}
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-
-                <div color="green">
-                <InputForm defaultValue={'Yes because its big'}/>
+            <div className='App-header'>
+                <div>
+                    logo
                 </div>
-
-                <InputForm defaultValue={'hello form'}/>
-
-            </header>
+                <div>
+                    <Link className='App-link' to={'/'}>Home</Link>
+                    <Link className='App-link' to={`/${routesPaths.about}`}>About</Link>
+                    <Link className='App-link' to={'/form'}>Form</Link>
+                    <Link className='App-link' to={'/policy'}>Policy</Link>
+                </div>
+            </div>
+            <Routes>
+                <Route path='/' element={<InputForm defaultValue={'Hello there!'}/>}/>
+                <Route path='*' element={<div>404</div>}/>
+                <Route path={`/${routesPaths.about}`} element={<div>About</div>}/>
+                <Route path='/policy' element={<PolicyPage/>}/>
+                <Route path='/form' element={<div>Form</div>}/>
+            </Routes>
         </div>
     );
 }
