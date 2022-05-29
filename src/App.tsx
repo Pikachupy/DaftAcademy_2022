93bac18a1f9113a94ddc83533a7b9b1316f7c6c5
Pikachupy <kala22051997@gmail.com>
2022-05-29 22:41:42 +0200

    Added navi and changed background

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,15 @@
 import React from 'react';
-import {Routes, Route, Link} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import './App.css';
 import {InputForm} from "./InputForm";
 import {PolicyPage} from "./policy";
-
-export const routesPaths = {
-    home: '/',
-    about: 'about',
-    form: 'form',
-    policy: 'policy'
-
-}
+import {routesPaths} from "./routes";
+import {Navigation} from "./navigation";
 
 function App() {
     return (
         <div className="App">
-            <div className='App-header'>
-                <div>
-                    logo
-                </div>
-                <div>
-                    <Link className='App-link' to={'/'}>Home</Link>
-                    <Link className='App-link' to={`/${routesPaths.about}`}>About</Link>
-                    <Link className='App-link' to={'/form'}>Form</Link>
-                    <Link className='App-link' to={'/policy'}>Policy</Link>
-                </div>
-            </div>
+            <Navigation/>
             <Routes>
                 <Route path={`/${routesPaths.home}`} element={<InputForm defaultValue={'Hello there!'}/>}/>
                 <Route path='*' element={<div>Error 404: you lost! </div>}/>
---
+++ src/routes/index.ts
@@ -0,0 +1,7 @@
+export const routesPaths = {
+    home: '/',
+    about: 'about',
+    form: 'form',
+    policy: 'policy'
+
+}
